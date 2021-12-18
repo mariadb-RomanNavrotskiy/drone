d608ec0cbcbf01027c5d4b1a5de43a8ec281f572
mariadb-RomanNavrotskiy <roman.navrotskiy@mariadb.com>
2021-12-18 18:30:30 +0200

    organize folder structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,52 +3,52 @@ provider "aws" {}
 resource "aws_instance" "drone" {
   instance_type          = "t2.small"
   ami                    = "ami-0323c3dd2da7fb37d"
-  subnet_id              = "${aws_subnet.drone.id}"
-  vpc_security_group_ids = ["${aws_security_group.drone.id}"]
+  subnet_id              = aws_subnet.drone.id
+  vpc_security_group_ids = [aws_security_group.drone.id]
   key_name               = "roman"
   iam_instance_profile   = "buildbot"
   root_block_device {
     volume_size           = 20
   }
   tags                   = {
-    Name = "drone2"
+    Name = "drone"
   }
 }
 
 resource "aws_eip" "drone" {
   vpc               = true
   tags = {
-    Name            = "drone2"
+    Name            = "drone"
   }
 }
 
 resource "aws_eip_association" "drone" {
-  instance_id   = "${aws_instance.drone.id}"
-  allocation_id = "${aws_eip.drone.id}"
+  instance_id   = aws_instance.drone.id
+  allocation_id = aws_eip.drone.id
 }
 
 resource "aws_vpc" "drone" {
-  cidr_block = "10.2.0.0/16"
+  cidr_block = "10.1.0.0/16"
   tags = {
-    Name = "drone2"
+    Name = "drone"
   }
 }
 
 resource "aws_internet_gateway" "drone" {
-  vpc_id = "${aws_vpc.drone.id}"
+  vpc_id = aws_vpc.drone.id
 }
 
 resource "aws_subnet" "drone" {
-  vpc_id            = "${aws_vpc.drone.id}"
-  cidr_block        = "10.2.1.0/24"
+  vpc_id            = aws_vpc.drone.id
+  cidr_block        = cidrsubnet(aws_vpc.drone.cidr_block, 8, 1)
   availability_zone = "us-east-1a"
   tags = {
-    Name = "drone2"
+    Name = "drone"
   }
 }
 
 resource "aws_security_group" "drone" {
-  vpc_id      = "${aws_vpc.drone.id}"
+  vpc_id      = aws_vpc.drone.id
 
   ingress {
     from_port   = 443
@@ -100,21 +100,25 @@ data "aws_route53_zone" "drone" {
 }
 
 resource "aws_route53_record" "drone" {
-  zone_id = "${data.aws_route53_zone.drone.zone_id}"
-  name    = "ci2.${data.aws_route53_zone.drone.name}"
+  zone_id = data.aws_route53_zone.drone.zone_id
+  name    = "ci.${data.aws_route53_zone.drone.name}"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_eip.drone.public_ip}"]
+  records = [aws_eip.drone.public_ip]
 }
 
 resource "aws_route53_record" "autoscaler" {
-  zone_id = "${data.aws_route53_zone.drone.zone_id}"
-  name    = "autoscaler2.${data.aws_route53_zone.drone.name}"
+  zone_id = data.aws_route53_zone.drone.zone_id
+  name    = "autoscaler.${data.aws_route53_zone.drone.name}"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_route53_record.drone.name}"]
+  records = [aws_route53_record.drone.name]
 }
 
-#output "drone_elastic_ip" {
-#  value = "${aws_eip.drone.public_ip}"
-#}
+resource "aws_route53_record" "autoscaler-arm" {
+  zone_id = data.aws_route53_zone.drone.zone_id
+  name    = "autoscaler-arm.${data.aws_route53_zone.drone.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_route53_record.drone.name]
+}
